0c8b9033e997e09d8e0d866df4cfd7473de7f62f
SettlementApp <aki01191017@gmail.com>
2020-06-25 13:23:32 +0900

    Initial Commit

---
+++ QiitaAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QiitaAPI
+//
+//  Created by 柿沼儀揚 on 2020/06/25.
+//  Copyright © 2020 柿沼儀揚. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
